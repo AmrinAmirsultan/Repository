6a034499d5830b648e2daf1d14933542f41a5fc2
kayato21 <lodzhyya@rambler.ru>
2023-04-01 11:57:04 +0600

    Add files

--- main.py
+++ main.py
@@ -13,23 +13,6 @@ def create_data_structures(name, age, city):
     my_dict = {"name": name, "age": age, "city": city}
     return my_list, my_tuple, my_dict
 
-
-#Напишите функцию в результате которая возвращает список, кортеж и словарь
-numbers = [1, 2, 3, 4, 5]
-def square(x):
-    return x ** 2
-squared_numbers = list(map(square, numbers))
-print(squared_numbers)  # [1, 4, 9, 16, 25]
-def is_even(x):
-    return x % 2 == 0
-even_numbers = list(filter(is_even, numbers))
-print(even_numbers)  # [2, 4]
-from functools import reduce
-def add(x, y):
-    return x + y
-sum_of_numbers = reduce(add, numbers)
-print(sum_of_numbers)  # 15
-
 #Приведите свой пример с использованием функций Map, Filter и Reduce. Выясните отличия.
 
 #map
@@ -51,23 +34,16 @@ print(sum)
 #Напишите функцию, принимающую в качестве параметра наименование улицы и стоимость товара и возвращающую общую сумму доставки
 
 def calculate_delivery_cost(street_name: str, product_price: float) -> float:
-    if not isinstance(street_name, str):
-        raise TypeError("street_name должен быть строкой")
-    if not isinstance(product_price, float):
-        raise TypeError("product_price должен быть числом типа float")
-
-    if all(substring in street_name for substring in ["Аль-Фараби", "Саина", "Ташенова", "Достык"]):
+    delivery_cost = 0.0
+    if "Аль-Фараби" in street_name and "Саина" in street_name and "Ташенова" in street_name and "Достык" in street_name:
         if product_price < 10000:
             delivery_cost = 500
-        else:
-            delivery_cost = 0
     else:
         if product_price < 10000:
             delivery_cost = 1000
         else:
             delivery_cost = 0
     return delivery_cost
-
 
 #Вызвать функцию внутри функции
 def compose(f, g):
